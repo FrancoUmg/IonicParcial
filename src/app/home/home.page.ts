ebd72a9ac6ce7d4e1b7706f61e948fefa0789c01
mike <mikeramirez-14-31@hotmail.com>
2019-05-03 15:39:31 -0600

    Segundo Parcial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PersonaService } from '../service/persona.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,70 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  data: any;
+
+  constructor( 
+    public personaService: PersonaService
+    ){
+      this.data ={
+        nombre: '',
+        apellido: '',
+        edad: '',
+        pais: ''
+      }
+  }
+
+  enviarForm(data) {
+    console.log(data)
+
+    let datosAEnviar: any = {
+      primerNombre: data.nombre,
+      segundoNombre: data.apellido,
+      edad: data.edad,
+      pais: data.pais
+    };
+
+    console.log('Datos a enviar:' + JSON.stringify(datosAEnviar));
+
+    this.personaService.create(datosAEnviar).subscribe(result => {
+      console.log('Datos from server:' + JSON.stringify(result));
+    });
+  }
+
+  actualizarForm(data) {
+    console.log(data)
+  
+    let datosAEnviar: any = {
+      primerNombre: data.nombre,
+      segundoNombre: data.apellido,
+      edad: data.edad,
+      pais: data.pais
+    };
+
+    console.log('Datos a enviar:' + JSON.stringify(datosAEnviar));
+
+    this.personaService.update(datosAEnviar).subscribe(result => {
+      console.log('Datos from server:' + JSON.stringify(result));
+    });
+
+  }
+
+
+MostrarDatos(data) {
+  console.log(data);
+  
+  const datosAEnviar: any = {
+  primerNombre: data.nombre,
+  segundoNombre: data.apellido,
+  edad: data.edad,
+  pais: data.pais
+  };
+  console.log('Todos los registros:' + JSON.stringify(datosAEnviar));
+  
+  this.personaService.getAll(datosAEnviar).subscribe(result => {
+  console.log('Datos from server:' + JSON.stringify(result));
+  });
+}
 
 }
